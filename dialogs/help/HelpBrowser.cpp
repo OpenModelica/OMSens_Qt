54183d588da0cfca143fc412f4d3876fd2da4f19
adanos <adanos@example.com>
2018-10-24 18:38:32 -0300

    help browser: added min width and height

--- dialogs/help/HelpBrowser.cpp
+++ dialogs/help/HelpBrowser.cpp
@@ -5,13 +5,19 @@
 HelpBrowser::HelpBrowser(const QString &textPath, QWidget *parent)
     : QDialog(parent)
 {
-    setAttribute(Qt::WA_DeleteOnClose);
 
+    // Initialize text browser
     textBrowser = new QTextBrowser;
+    textBrowser->setSource(textPath);
 
+
+    // Layout
     QVBoxLayout *pMainLayout = new QVBoxLayout;
     pMainLayout->addWidget(textBrowser);
     setLayout(pMainLayout);
 
-    textBrowser->setSource(textPath);
+    // Window attributes
+    setAttribute(Qt::WA_DeleteOnClose);
+    setMinimumHeight(900);
+    setMinimumWidth(800);
 }
