4055f1dd8ae14056d4222b81603dd7873347f112
adanos <adanos@example.com>
2019-01-29 15:31:04 -0300

    variables tab: added new constructor with predefined checks

--- tabs/VariablesTab.cpp
+++ tabs/VariablesTab.cpp
@@ -5,9 +5,31 @@
 #include <QHeaderView>
 #include <QLabel>
 
+// Constructors
+// Default constructor
 VariablesTab::VariablesTab(QList<QString> variables, QWidget *parent) : QWidget(parent)
 {
-    //Initialize table header
+    // Define default checks for variables
+    QList<VariableToInclude> vars_to_include;
+    foreach (QString variable, variables)
+    {
+        bool default_check = true;
+        VariableToInclude var_include = VariableToInclude(variable,default_check);
+        vars_to_include.append(var_include);
+    }
+    // Initialize
+    initialize(vars_to_include);
+}
+
+// Custom constructor
+VariablesTab::VariablesTab(QList<VariableToInclude> vars_to_include, QWidget *parent) : QWidget(parent)
+{
+    initialize(vars_to_include);
+}
+
+// Initialize
+void VariablesTab::initialize(QList<VariableToInclude> vars_to_include)
+{
     mpVariablesTable = new QTableWidget(0, 2);
     const QList<QString> tableHeaders( QList<QString>()
                                      << "Variable"
@@ -15,21 +37,21 @@ VariablesTab::VariablesTab(QList<QString> variables, QWidget *parent) : QWidget(
                                      );
     mpVariablesTable->setHorizontalHeaderLabels(tableHeaders);
 
-    foreach (QString variable, variables)
+    foreach (VariableToInclude var_include, vars_to_include)
     {
-        // Add a rowº
+        // Add a row
         // Row index to add row to
         const int rowNum = mpVariablesTable->rowCount();
         // Add "blank" row
         mpVariablesTable->insertRow(rowNum);
         // Fill blank row with values corresponding to this variable
         // Set variable name
-        QLabel *varNameWidget = new QLabel(variable);
+        QLabel *varNameWidget = new QLabel(var_include.name);
         mpVariablesTable->setCellWidget(rowNum,varColPos, varNameWidget);
         // Set checkbox
         // Wrap the checkbox in a generic QWidget so we can set a layout to center it in the cell
         QCheckBox *includeCheckBox = new QCheckBox;
-        includeCheckBox->setChecked(true);
+        includeCheckBox->setChecked(var_include.include);
         mpVariablesTable->setCellWidget(rowNum,cboxColPos,includeCheckBox);
     }
     QVBoxLayout *mainLayout = new QVBoxLayout;
@@ -37,6 +59,9 @@ VariablesTab::VariablesTab(QList<QString> variables, QWidget *parent) : QWidget(
     setLayout(mainLayout);
 }
 
+
+
+// Methods
 QTableWidget *VariablesTab::getVariablesTable() const
 {
     return mpVariablesTable;
--- tabs/VariablesTab.h
+++ tabs/VariablesTab.h
@@ -4,11 +4,21 @@
 #include <QWidget>
 #include <QTableWidget>
 
+// Aux Structs
+struct VariableToInclude{
+  QString name;
+  bool include;
+
+  VariableToInclude(QString name, bool include): name(name), include(include){}
+};
+
+// Class
 class VariablesTab : public QWidget
 {
     Q_OBJECT
 public:
     explicit VariablesTab(QList<QString> variables, QWidget *parent = 0);
+    explicit VariablesTab(QList<VariableToInclude> vars_to_include, QWidget *parent = 0);
     // Getters
     QTableWidget *getVariablesTable() const;
     // Conventions:
@@ -18,6 +28,8 @@ public:
 
 private:
     QTableWidget *mpVariablesTable;
+    void initialize(QList<VariableToInclude> vars_to_include);
+
 signals:
 
 public slots:
