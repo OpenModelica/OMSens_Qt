1a56f5576656b11ab852f47f5672f6072e0ff437
Adeel Asghar <adeel.asghar@liu.se>
2020-01-24 13:19:52 +0100

    Fix Linux compilation errors

--- ForceSignDoubleSpinbox.h
+++ ForceSignDoubleSpinbox.h
@@ -6,15 +6,14 @@
 class ForceSignDoubleSpinbox: public QDoubleSpinBox
 {
 public:
-    using QDoubleSpinBox::QDoubleSpinBox;
-    QString textFromValue(double value) const override
-    {
-        // Adapt the display of text to always show sign (+ or -)
-        QString text = QDoubleSpinBox::textFromValue(value);
-        if(value > 0)
-            text.prepend(QChar('+'));
-        return text;
-    }
+  QString textFromValue(double value) const override
+  {
+    // Adapt the display of text to always show sign (+ or -)
+    QString text = QDoubleSpinBox::textFromValue(value);
+    if(value > 0)
+      text.prepend(QChar('+'));
+    return text;
+  }
 };
 
 #endif // DOUBLESPINBOXADAPTED_H
